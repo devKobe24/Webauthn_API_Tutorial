ddf731c8d7890a75707df6ce28340915de595b5f
Kobe <dev.skyachieve91@gmail.com>
2023-11-10 16:10:12 +0900

    :sparkles: Feat: CreateUsers 마이그레이션.

--- Sources/App/Migrations/CreateUsers.swift
+++ Sources/App/Migrations/CreateUsers.swift
@@ -1,8 +1,24 @@
 //
-//  File.swift
-//  
+//  CreateUsers.swift
+//
 //
 //  Created by Minseong Kang on 11/10/23.
 //
 
-import Foundation
+import Fluent
+
+struct CreateUsers: AsyncMigration {
+    func prepare(on database: FluentKit.Database) async throws {
+        try await database.schema("users")
+            .id()
+            .field("username", .string, .required)
+            .field("password_hash", .string)
+            .field("created_at", .datetime, .required)
+            .unique(on: "username")
+            .create()
+    }
+    
+    func revert(on database: FluentKit.Database) async throws {
+        try await database.schema("users").delete()
+    }
+}
