556cfd299b34f5ad0da80b4330075e21ee3925b5
Kobe <dev.skyachieve91@gmail.com>
2023-11-12 16:06:12 +0900

    :recycle: Refactor: SignUp Endpoint Routes 생성 및 구현.

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -20,4 +20,18 @@ func routes(_ app: Application) throws {
         response.headers.contentType = HTTPMediaType(type: "application", subType: "json")
         return response
     }
+    
+    let authSessionRoute = app.grouped(User.sessionAuthenticator())
+    
+    authSessionRoute.get("signup") { req -> Response in
+        let username = try req.query.get(String.self, at: "username")
+        guard try await User.query(on: req.db).filter(\.$username == username).first() == nil else {
+            throw Abort(.conflict, reason: "Username is already taken")
+        }
+        
+        let user = User(username: username)
+        try await user.create(on: req.db)
+        req.auth.login(user)
+        return req.redirect(to: "makeCredential")
+    }
 }
